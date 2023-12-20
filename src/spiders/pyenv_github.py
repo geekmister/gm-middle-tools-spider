2e650f083c89578f9e900118ebaf11f8d66e79fc
Geekmister <geekmister.mg@gmail.com>
2023-12-20 17:37:53 +0800

    feat(spider): test scrapy

---
+++ src/spiders/pyenv_github.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+from scrapy import Spider
+from scrapy.http import Response
+
+
+class PyenvGithubSpider(Spider):
+    name = "pyenv_github_spider"
+    start_urls = ["https://github.com/pyenv/pyenv/releases"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        list_selector = response.xpath("//a[@class='Link--primary Link']")
+        for one_selector in list_selector:
+            version = one_selector.xpath("text()")
+            print("玛德")
+            print(version)
+
+        print(list_selector)
+
